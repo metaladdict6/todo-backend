114b35142317a9e258b386021a43cc4ed9f14807
Robert <r.r.c.ziengs@gmail.com>
2024-07-21 12:06:15 +0200

    update
    updates

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -1,10 +1,11 @@
 package models;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
